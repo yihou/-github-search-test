bbc6f713490240c8cabcb3088834185cceffac4b
Yi Hou <looyihou91@gmail.com>
2020-06-25 04:03:37 +0800

    turn next to ts-next

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -4,14 +4,12 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Test App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Test</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
@@ -47,17 +45,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
 
       <style jsx>{`
         .container {
@@ -190,7 +177,7 @@ export default function Home() {
         }
       `}</style>
 
-      <style jsx global>{`
+      <style jsx>{`
         html,
         body {
           padding: 0;
